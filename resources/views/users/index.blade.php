7b920c9859e086f405c7dc8d87ce2c0c7d97c2bb
frank collins <frank@localhost.localdomain>
2019-12-10 15:01:01 -0500

    users and roles

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -95,100 +95,77 @@
 
         <form class="form-horizontal" role="form" method="POST" action="{{ url('admin/users') }}">
             {{ csrf_field() }}
-<div class="row"></div>
-            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                <label for="name" class="col-md-4 control-label">Name</label>
+<div class="row">
+    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }} col-md-6">
+        <label for="name" class="control-label">Name:</label>
 
-                <div class="col-md-6">
-                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}"
-                           required autofocus>
+            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}"
+                   required autofocus>
 
-                    @if ($errors->has('name'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('name') }}</strong>
-                    </span>
-                    @endif
-                </div>
-            </div>
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                <label for="email" class="col-md-4 control-label">Email:</label>
+            @if ($errors->has('name'))
+                <span class="help-block">
+                <strong>{{ $errors->first('name') }}</strong>
+            </span>
+            @endif
+    </div>
+    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} col-md-6">
+        <label for="email" class="control-label">Email:</label>
 
-                <div class="col-md-6">
-                    <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}"
-                           required autofocus>
+            <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}"
+                   required autofocus>
 
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                    @endif
-                </div>
-            </div>
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label for="password" class="col-md-4 control-label">Password</label>
+            @if ($errors->has('email'))
+                <span class="help-block">
+                <strong>{{ $errors->first('email') }}</strong>
+            </span>
+            @endif
+    </div>
+    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} col-md-6">
+        <label for="password" class="control-label">Password:</label>
+            <input id="password" type="password" class="form-control" name="password" required autofocus>
+            @if ($errors->has('password'))
+                <span class="help-block">
+                <strong>{{ $errors->first('password') }}</strong>
+            </span>
+            @endif
+    </div>
 
-                <div class="col-md-6">
-                    <input id="password" type="password" class="form-control" name="password" required autofocus>
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                    @endif
-                </div>
-            </div>
+    <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }} col-md-6">
+        <label for="password_confirmation" class="col-md-4 control-label">Confirm</label>
 
-            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                <label for="password_confirmation" class="col-md-4 control-label">Confirm Password</label>
+            <input id="password_confirmation" type="password" placeholder="Confirm Password" class="form-control" name="password_confirmation" required autofocus>
+            @if ($errors->has('password_confirmation'))
+                <span class="help-block">
+                <strong>{{ $errors->first('password_confirmation') }}</strong>
+            </span>
+            @endif
+    </div>
+</div>
+<div class="form-group{{ $errors->has('roles') ? ' has-error' : '' }}">
+    <label for="roles" class="col-md-4 control-label">Roles</label>
 
-                <div class="col-md-6">
-                    <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required autofocus>
-                    @if ($errors->has('password_confirmation'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                    </span>
-                    @endif
-                </div>
-            </div>
-
-            <div class="form-group{{ $errors->has('roles') ? ' has-error' : '' }}">
-                <label for="roles" class="col-md-4 control-label">Roles</label>
-
-                <div class="col-md-6">
-                    <select id="role" name="roles[]" multiple>
-                    @foreach ($roles as $key => $role)
-                            <option value="{{$key}}">{{$role}}</option>
-                    @endforeach
-                    </select>
+    <div class="col-md-10">
+        <select id="role" name="roles[]" style="width:700px" multiple>
+        @foreach ($roles as $key => $role)
+                <option value="{{$key}}">{{$role}}</option>
+        @endforeach
+        </select>
 
 
-                    @if ($errors->has('roles'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('roles') }}</strong>
-                    </span>
-                    @endif
-                </div>
-            </div>
-
-
-
-            <div class="form-group">
-                <div class="col-md-12 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary">
-                        Save
-                    </button>
-
-                    <a class="btn btn-link" href="{{ url('admin/users') }}">
-                        Cancel
-                    </a>
-                </div>
-            </div>
+        @if ($errors->has('roles'))
+            <span class="help-block">
+            <strong>{{ $errors->first('roles') }}</strong>
+        </span>
+        @endif
+    </div>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    <button type="submit" class="btn btn-primary">Save changes</button>
+  </div>
         </form>
 
          </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
       </div>
     </div>
   </div>
